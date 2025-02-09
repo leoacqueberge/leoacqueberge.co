087804b49ad9631905bc6491c20cfefcc679e1e2
leoacqueberge <132593994+leoacqueberge@users.noreply.github.com>
2025-02-09 02:35:46 +0100

    First commit

---
+++ .next/static/webpack/app-pages-internals.93fdce126f543098.hot-update.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("app-pages-internals",{
+
+/***/ "(app-pages-browser)/./public/content/media/profilePhoto.jpg":
+/*!***********************************************!*\
+  !*** ./public/content/media/profilePhoto.jpg ***!
+  \***********************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval(__webpack_require__.ts("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({\"src\":\"/_next/static/media/profilePhoto.55d12d12.jpg\",\"height\":500,\"width\":500,\"blurDataURL\":\"/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FprofilePhoto.55d12d12.jpg&w=8&q=70\",\"blurWidth\":8,\"blurHeight\":8});//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1wYWdlcy1icm93c2VyKS8uL3B1YmxpYy9jb250ZW50L21lZGlhL3Byb2ZpbGVQaG90by5qcGciLCJtYXBwaW5ncyI6Ijs7OztBQUFBLGlFQUFlLENBQUMsNE1BQTRNIiwic291cmNlcyI6WyIvVXNlcnMvbGVvYWNxdWViZXJnZS9Eb3dubG9hZHMvcmVhZGN2LW5leHRqcy1leHBvcnQtZmViLTgtMjAyNS9wdWJsaWMvY29udGVudC9tZWRpYS9wcm9maWxlUGhvdG8uanBnIl0sInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBkZWZhdWx0IHtcInNyY1wiOlwiL19uZXh0L3N0YXRpYy9tZWRpYS9wcm9maWxlUGhvdG8uNTVkMTJkMTIuanBnXCIsXCJoZWlnaHRcIjo1MDAsXCJ3aWR0aFwiOjUwMCxcImJsdXJEYXRhVVJMXCI6XCIvX25leHQvaW1hZ2U/dXJsPSUyRl9uZXh0JTJGc3RhdGljJTJGbWVkaWElMkZwcm9maWxlUGhvdG8uNTVkMTJkMTIuanBnJnc9OCZxPTcwXCIsXCJibHVyV2lkdGhcIjo4LFwiYmx1ckhlaWdodFwiOjh9OyJdLCJuYW1lcyI6W10sImlnbm9yZUxpc3QiOltdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///(app-pages-browser)/./public/content/media/profilePhoto.jpg\n"));
+
+/***/ })
+
+});
---
+++ public/content/writing/YRXMhwv85RDKvd7xOMw8.md
@@ -0,0 +1,40 @@
+---
+title: Ui Design at Ledgity
+description: By Léo Acqueberge
+---
+## My role
+
+UI/UX Design - Graphic Design - Project Management (Design & Marketing)
+
+**Tools** : Figma, Blender, After Effects
+
+## The Problem
+
+Stepping into the fintech domain for the first time, I encountered unfamiliar territory. This marked my initial exposure to the industry, allowing me to quickly adapt and learn.
+
+This role marked my inaugural experience in UI design. Collaborating with my colleagues, I embraced the learning curve enthusiastically, gaining invaluable insights and contributing to a highly enriching journey.
+
+## App
+
+Working on creating new screens layouts, applying the Ledgity brand guidelines consistently across all screens and changing patterns as and when needed.
+
+![Concept I made, proposed, and successfully implemented in the Ledgity app.](/content/writing/YRXMhwv85RDKvd7xOMw8-1.jpg)
+![User switch loading screen (client to pro)](/content/writing/YRXMhwv85RDKvd7xOMw8-2.mp4)
+![Bts of the 3d Ledgity Logo](/content/writing/YRXMhwv85RDKvd7xOMw8-3.jpg)
+
+## Branding (Ledgity Yield)
+
+I was responsible for overseeing the branding of Ledgity Yield, a cryptocurrency project. During this role, I collaborated closely with blockchain developers and the marketing team, which was responsible for communication during the launch of Ledgity's crypto platform. These diverse and creativity-rich experiences provided me with invaluable learning opportunities.
+
+![LDY Token branding identity](/content/writing/YRXMhwv85RDKvd7xOMw8-4.gif)
+
+## Design System
+
+I took charge to made a design system to keep consistency and speed up the design workflow.
+
+![](/content/writing/YRXMhwv85RDKvd7xOMw8-5.jpg)
+
+## Learnings
+
+**"Being versatile"**\
+When presented with multiple visual directions exploring how they could work in various scenarios and features direction to take was a valuable exercise.
