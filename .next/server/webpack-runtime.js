5953f893a5dee54973db02acdf4a32f99d6c9477
Léo Acqueberge <132593994+leoacqueberge@users.noreply.github.com>
2025-06-28 04:06:21 +0200

    Remove obsolete image cache files, update webpack cache files, and enhance project descriptions in profileData.json for improved clarity and user engagement.

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("f6668bbf31bab98a")
+/******/ 		__webpack_require__.h = () => ("f0278b4f9d27af98")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
